029f1405eec30932e29ba0642f5742529ddb43f8
Tim <herew26@gmail.com>
2023-04-11 12:20:39 +0300

    feat: add app command

--- src/main/java/timnekk/App.java
+++ src/main/java/timnekk/App.java
@@ -1,14 +1,29 @@
 package timnekk;
 
-public class App {
-    /**
-     * @param args
-     */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.ParameterException;
+
+@Command(name = "chatter-server", subcommands = {
+        StartCommand.class }, mixinStandardHelpOptions = true, description = "Chatter server", abbreviateSynopsis = true)
+public class App implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    @Override
+    public void run() {
+    }
+
     public static void main(String[] args) {
-        try (Server server = new Server(9999)) {
-            server.run();
-        } catch (Exception e) {
-            // TODO: handle
+        final CommandLine cmd = new CommandLine(new App());
+
+        try {
+            cmd.parseArgs(args);
+            cmd.execute(args);
+        } catch (ParameterException e) {
+            logger.error("Parsing command line arguments failed: {}", e.getMessage());
         }
     }
 }
